429d9d815d7eaf326097cd8b7593ced2c5c1c1c7
Md Mahfuj Hossain <130858175+mahfuj4728@users.noreply.github.com>
2025-02-25 16:18:42 +0600

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(BloodDonationApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class BloodDonationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
